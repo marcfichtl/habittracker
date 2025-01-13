6346acf66594c5d5c4bb18afe5833818f64c84f0
frvnzz <franzaurel.huber@icloud.com>
2025-01-13 16:09:46 +0100

    AddScreen.kt start

--- app/src/main/java/com/example/habittracker/ui/AddScreen.kt
+++ app/src/main/java/com/example/habittracker/ui/AddScreen.kt
@@ -1,2 +1,31 @@
 package com.example.habittracker.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AddScreen() {
+    Column {
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.LightGray,
+                    shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp)
+                )
+        ) {
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Habit Name") }
+            )
+        }
+    }
+}
