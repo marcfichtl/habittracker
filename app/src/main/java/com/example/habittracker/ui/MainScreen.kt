05301b204f0bffcfa57150c547c745585e2b4f96
Marcus Fichtinger <cc231016@fhstp.ac.at>
2025-01-13 14:49:44 +0100

    Navigation changes and added Icons

--- app/src/main/java/com/example/habittracker/ui/MainScreen.kt
+++ app/src/main/java/com/example/habittracker/ui/MainScreen.kt
@@ -9,20 +9,35 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Screens(val route: String) {
+    Main("Main"),
+    Add("Add"),
+    Edit("edit/{habitId}")
+}
 
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
     dataViewModel: DataViewModel = viewModel(factory = ViewModelFactoryProvider.Factory)
 ){
-    val state by dataViewModel.habitsUiState.collectAsStateWithLifecycle() //
+    val navController = rememberNavController()
+    val state by dataViewModel.habitsUiState.collectAsStateWithLifecycle()
 
-    Button(
-        onClick = ({ /*TODO*/ })
-    ) {
-        Icon(
-            imageVector = Icons.Default.Add,
-            contentDescription = "Add"
-        )
+    NavHost(navController, Screens.Main.route,
+        modifier = modifier
+    ){
+        composable(Screens.Main.route) {
+            TODO("Display the Habits on a List")
+        }
+        composable(Screens.Add.route) {
+            TODO("Add a new Habit")
+        }
+        composable(Screens.Edit.route) {
+            TODO("Edit a Habit")
+        }
     }
 }
