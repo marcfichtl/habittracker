122e09bce4a784dd80094fc3e4a274abc23df36f
frvnzz <franzaurel.huber@icloud.com>
2025-01-13 12:13:46 +0100

    HabitApplication.kt edited

--- app/src/main/java/com/example/habittracker/HabitApplication.kt
+++ app/src/main/java/com/example/habittracker/HabitApplication.kt
@@ -1,4 +1,13 @@
 package com.example.habittracker
 
+import android.app.Application
+import com.example.habittracker.data.HabitRepository
+import com.example.habittracker.data.db.HabitDatabase
+
 class HabitApplication {
+    val habitRepository by lazy {
+        val dao = HabitDatabase.getDatabase(this).habitDao()
+
+        HabitRepository(dao)
+    }
 }
