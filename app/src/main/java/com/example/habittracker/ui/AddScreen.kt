fb5e320cd229b92c410ffa210133c16dbc4c7d1a
Marcus Fichtinger <cc231016@fhstp.ac.at>
2025-01-14 11:02:14 +0100

    Updated AddScreen

--- app/src/main/java/com/example/habittracker/ui/AddScreen.kt
+++ app/src/main/java/com/example/habittracker/ui/AddScreen.kt
@@ -1,6 +1,5 @@
 package com.example.habittracker.ui
 
-import android.content.res.Resources.Theme
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -8,9 +7,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,7 +18,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -33,7 +31,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 val colorOptions = listOf(
     Color(0xFFf48b94),
@@ -64,7 +64,7 @@ fun AddScreen() {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(32.dp)
             ) {
                 Text("Habit Name")
                 TextField(
@@ -93,21 +93,43 @@ fun AddScreen() {
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(0.7f)
+                .padding(top = 56.dp, start = 32.dp, end = 32.dp)
         ) {
             Spacer(modifier = Modifier.height(16.dp))
-            Box(
+            Row(
                 modifier = Modifier
-                    .size(50.dp)
-                    .clip(RoundedCornerShape(50))
-                    .background(selectedColor)
-                    .clickable { showDialog = true }
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Choose Color",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Box(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(RoundedCornerShape(50))
+                        .background(selectedColor)
+                        .clickable { showDialog = true }
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color.Gray)
+                    .padding(bottom = 16.dp)
             )
+
         }
 
         if (showDialog) {
             AlertDialog(
                 onDismissRequest = { showDialog = false },
-                title = { Text("Select Color") },
+                title = { Text("Choose Color") },
                 text = {
                     LazyVerticalGrid(
                         columns = GridCells.Fixed(4),
