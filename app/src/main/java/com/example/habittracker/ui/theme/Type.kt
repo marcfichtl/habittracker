69f0c68b62427904bf0371dd79ea213fc17aabc0
frvnzz <franzaurel.huber@icloud.com>
2025-01-14 12:01:06 +0100

    Type.kt updated for the new font

--- app/src/main/java/com/example/habittracker/ui/theme/Type.kt
+++ app/src/main/java/com/example/habittracker/ui/theme/Type.kt
@@ -2,14 +2,17 @@ package com.example.habittracker.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.habittracker.R
 
-// Set of Material typography styles to start with
+val RegularFont = FontFamily(Font(R.font.regular_font))
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = RegularFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
@@ -17,14 +20,14 @@ val Typography = Typography(
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = RegularFont,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = RegularFont,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
